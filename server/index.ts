90c68e55d08ed5c27fe3b971228e6b49a2bd10b3
Kodai <0711dr5@gmail.com>
2024-04-23 15:22:06 +0900

    fix:express-validatorインストール

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,12 @@
 import AES from 'crypto-js/aes'
 import dotenv from 'dotenv'
 import express from 'express'
+import { body, validationResult } from 'express-validator'
 import type { Express, Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
-
 import { User } from './src/v1/models/user'
+
 dotenv.config({ path: './.env.local' })
 
 const app: Express = express()
@@ -38,29 +39,39 @@ mongoose
   })
 
 //ユーザー新規登録API
-app.post('/register', async (req: Request, res: Response) => {
-  // パスワードの受け取り
-  const password = req.body.password
+app.post(
+  '/register',
+  body('username')
+    .isLength({
+      min: 8,
+    })
+    .withMessage('ユーザー名は8文字以上である必要があります'),
+  body('password').isLength({ min: 8 }).withMessage('パスワードは8文字以上である必要があります'),
+  body('confirmPassword').isLength({ min: 8 }).withMessage('確認パスワードは8文字以上である必要があります'),
+  async (req: Request, res: Response) => {
+    // パスワードの受け取り
+    const password = req.body.password
 
-  try {
-    // パスワードの暗号化
-    req.body.password = AES.encrypt(password, SECRET_KEY as string)
+    try {
+      // パスワードの暗号化
+      req.body.password = AES.encrypt(password, SECRET_KEY as string)
 
-    // ユーザーの新規登録
-    const user = await User.create(req.body)
+      // ユーザーの新規登録
+      const user = await User.create(req.body)
 
-    // JWTの生成
-    const token = jwt.sign(
-      {
-        id: user._id,
-      },
-      TOKEN_SECRET_KEY as string,
-      {
-        expiresIn: '24h',
-      },
-    )
-    return res.status(200).json({ user, token })
-  } catch (error) {
-    return res.status(500).json(error)
-  }
-})
+      // JWTの生成
+      const token = jwt.sign(
+        {
+          id: user._id,
+        },
+        TOKEN_SECRET_KEY as string,
+        {
+          expiresIn: '24h',
+        },
+      )
+      return res.status(200).json({ user, token })
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+  },
+)
