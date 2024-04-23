dbc2f0903be860a4991ed6f4685b0b2cbbd00090
Kodai <0711dr5@gmail.com>
2024-04-23 13:56:08 +0900

    fix:環境変数

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,26 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env.local" });
 
 const app: Express = express();
 const port = 3001;
 
-const uri =
-  "mongodb+srv://0711dr5:O841sfqrMH9HlcxD@cluster0.ilvc0j8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const url = process.env.MONGODB_URI;
+
+if (!url) {
+  throw new Error("The MongoDB URI is not defined.");
+}
 
 // Connect to MongoDB
-try {
-  mongoose.connect(uri);
-} catch (error) {
-  console.error(error);
-}
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
