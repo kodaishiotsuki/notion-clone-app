3e6a178bc79b55fd39c71981cd6a4af78a117a77
Kodai <0711dr5@gmail.com>
2024-04-26 16:41:57 +0900

    fix:eslint + prettier

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -5,14 +5,40 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended', // Add this line at the end
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'import',
+    'prettier', // Add 'prettier' to the plugins array
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
     ],
+    'import/newline-after-import': ['error', { count: 1 }],
+    'import/no-duplicates': ['error'],
+    'prettier/prettier': ['error'], // Ensure Prettier rules are treated as errors
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 }
