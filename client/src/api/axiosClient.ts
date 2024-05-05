65b1826bae5cee811d4acac38ddb457af8fc18a8
Kodai <0711dr5@gmail.com>
2024-05-05 11:24:09 +0900

    fix:axiox

--- client/src/api/axiosClient.ts
+++ client/src/api/axiosClient.ts
@@ -1,10 +1,32 @@
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:3001/api/v1'
+const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 })
+
+// APIを叩く前に前処理を行う
+axiosClient.interceptors.request.use(async (config) => {
+  // headers を設定
+  config.headers['Content-Type'] = 'application/json'
+  const token = getToken()
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
+
+  return config
+})
+
+// APIを叩いた後に後処理を行う
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    throw error
+  },
+)
+
+export default axiosClient
