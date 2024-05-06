b89f6a94f3c6ae4d13b03aded2b32abc804f30fa
Kodai <0711dr5@gmail.com>
2024-05-06 11:16:45 +0900

    fix:サイドバー構築

--- client/src/components/common/Sidebar.tsx
+++ client/src/components/common/Sidebar.tsx
@@ -1,3 +1,85 @@
+import { AddBoxOutlined, LogoutOutlined } from '@mui/icons-material'
+import { Box, Drawer, IconButton, List, ListItemButton, Typography } from '@mui/material'
+
+import assets from '../../assets'
+
 export default function Sidebar() {
-  return <div>Sidebar</div>
+  return (
+    <Drawer
+      container={window.document.body}
+      variant='permanent'
+      open={true}
+      sx={{
+        width: 250,
+        height: '100vh',
+      }}
+    >
+      <List
+        sx={{
+          width: 250,
+          height: '100vh',
+          backgroundColor: assets.colors.secondary,
+        }}
+      >
+        <ListItemButton>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant='body2' fontWeight='700'>
+              UserName
+            </Typography>
+            <IconButton>
+              <LogoutOutlined />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+        <Box
+          sx={{
+            paddingTop: '10px',
+          }}
+        ></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant='body2' fontWeight='700'>
+              お気に入り
+            </Typography>
+          </Box>
+        </ListItemButton>
+        <Box
+          sx={{
+            paddingTop: '10px',
+          }}
+        ></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant='body2' fontWeight='700'>
+              プライベート
+            </Typography>
+            <IconButton>
+              <AddBoxOutlined fontSize='small' />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+      </List>
+    </Drawer>
+  )
 }
